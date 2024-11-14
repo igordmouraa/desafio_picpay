bcbcec952ab6d2022157d17c74f32a31bbc897a3
igordmouraa <igordmoura_@hotmail.com>
2024-11-14 19:11:54 -0300

    first commit

---
+++ src/test/java/br/com/igordmoura/desafio_picpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igordmoura.desafio_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

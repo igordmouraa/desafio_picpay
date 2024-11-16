312c0bf4238bd644b6d664da5a648c863caf07e4
igordmouraa <igordmoura_@hotmail.com>
2024-11-16 12:22:25 -0300

    Add TransferService with transaction and validation logic
    Introduced `TransferService` to handle transfer operations, including balance checks, wallet validation, and transaction management. Ensured async notification on successful transfer completion. This addition enhances the service layer by ensuring secure and validated fund transfers.

---
+++ src/main/java/br/com/igordmoura/desafio_picpay/service/TransferService.java
@@ -0,0 +1,73 @@
+package br.com.igordmoura.desafio_picpay.service;
+
+import br.com.igordmoura.desafio_picpay.Repository.TransferRepository;
+import br.com.igordmoura.desafio_picpay.Repository.WalletRepository;
+import br.com.igordmoura.desafio_picpay.controller.dto.TransferDto;
+import br.com.igordmoura.desafio_picpay.entity.Transfer;
+import br.com.igordmoura.desafio_picpay.entity.Wallet;
+import br.com.igordmoura.desafio_picpay.exception.InsufficientBalanceException;
+import br.com.igordmoura.desafio_picpay.exception.TransferNotAllowedForWalletTypeException;
+import br.com.igordmoura.desafio_picpay.exception.TransferNotAuthorizedException;
+import br.com.igordmoura.desafio_picpay.exception.WalletNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class TransferService {
+
+    private final TransferRepository transferRepository;
+    private final AuthorizationService authorizationService;
+    private final NotificationService notificationService;
+    private final WalletRepository walletRepository;
+
+    public TransferService(TransferRepository transferRepository, AuthorizationService authorizationService, NotificationService notificationService, WalletRepository walletRepository) {
+        this.transferRepository = transferRepository;
+        this.authorizationService = authorizationService;
+        this.notificationService = notificationService;
+        this.walletRepository = walletRepository;
+    }
+
+    @Transactional
+    public Transfer transfer(TransferDto transferDto) {
+
+        var sender = walletRepository.findById(transferDto.payer())
+                .orElseThrow(() -> new WalletNotFoundException(transferDto.payer()));
+
+        var receiver = walletRepository.findById(transferDto.payee())
+                .orElseThrow(() -> new WalletNotFoundException(transferDto.payee()));
+
+        validateTransfer(transferDto, sender);
+
+        sender.debit(transferDto.value());
+        receiver.credit(transferDto.value());
+
+        var transfer = new Transfer(sender, receiver, transferDto.value());
+
+        walletRepository.save(sender);
+        walletRepository.save(receiver);
+
+        var transferResult = transferRepository.save(transfer);
+
+        CompletableFuture.runAsync(() -> notificationService.sendNotification(transferResult));
+
+        return transferResult;
+    }
+
+    public void validateTransfer(TransferDto transferDto, Wallet sender) {
+        if (!sender.isTransferAllowedForWalletType()) {
+            throw new TransferNotAllowedForWalletTypeException();
+        }
+
+        if(!sender.isBalanceEqualOrGreaterThan(transferDto.value())) {
+            throw new InsufficientBalanceException();
+        }
+
+        if(!authorizationService.isAuthorized(transferDto)) {
+            throw new TransferNotAuthorizedException();
+        }
+
+    }
+}
